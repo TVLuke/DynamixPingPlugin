341f08f69372fd5bd05abbc3da00cc69ffa55d8d
Lukas Ruge <ruge@itm.uni-luebeck.de>
2013-08-15 10:56:17 +0200

    update

--- src/org/ambientdynamix/contextplugins/ping/PingContextInfo.java
+++ src/org/ambientdynamix/contextplugins/ping/PingContextInfo.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) Institute of Telematics, Lukas Ruge
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.ambientdynamix.contextplugins.ping;
 
 import java.util.Date;
@@ -10,6 +26,13 @@ import android.os.Parcel;
 import android.os.Parcelable;
 import android.util.Log;
 
+/**
+ * The PingContextInfo is a simple extension of the standard IContextInfo Interface. It receives a String and stores this to be retreived.
+ * 
+ * 
+ * @author lukas
+ *
+ */
 public class PingContextInfo implements IContextInfo
 {
 
--- src/org/ambientdynamix/contextplugins/ping/PingPluginRuntime.java
+++ src/org/ambientdynamix/contextplugins/ping/PingPluginRuntime.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) Institute of Telematics, Lukas Ruge
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.ambientdynamix.contextplugins.ping;
 
 import java.util.Date;
@@ -12,7 +28,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
-
+/**
+ * The PingPluginRuntime only reacts to configured context requests. It has to receive a string in a Bundle with the key "id". 
+ * It will create a PingContextInfo Object and send it via Dynamix
+ *
+ * This is an example plug-in that is very simple and can be used as a starting point for plug-in-development. 
+ * 
+ * It is also used for roundtrip-time testing of dynamix
+ * 
+ * @author lukas
+ *
+ */
 public class PingPluginRuntime extends AutoReactiveContextPluginRuntime
 {
 	private final String TAG = "PINGPLUGIN";
@@ -72,7 +98,7 @@ public class PingPluginRuntime extends AutoReactiveContextPluginRuntime
 		sendContextEvent(requestId, aci);
 		d = new Date();
 		//Log.d(TAG, "Send Context: "+d.getTime());
-		Log.d(TAG, "time: "+(d.getTime()-x1)+ "ms");
+		Log.d(TAG, id+" - time: "+(d.getTime()-x1)+ "ms");
 		context=this;
 	}
 
