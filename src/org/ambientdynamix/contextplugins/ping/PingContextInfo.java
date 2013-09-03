75841d1c184a6a6aa83aaf153cf1236eeffa3f99
Lukas Ruge <ruge@itm.uni-luebeck.de>
2013-09-03 13:21:30 +0200

    Update

--- src/org/ambientdynamix/contextplugins/ping/PingContextInfo.java
+++ src/org/ambientdynamix/contextplugins/ping/PingContextInfo.java
@@ -33,7 +33,7 @@ import android.util.Log;
  * @author lukas
  *
  */
-public class PingContextInfo implements IContextInfo, IPingContextInfo
+public class PingContextInfo implements IContextInfo
 {
 
 	private final String TAG = "PINGPLUGIN";
